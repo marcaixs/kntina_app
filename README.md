d5474450daf7b9c78c7e3199bb69c528c8b87125
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-22 10:35:14 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# kntina_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/kntina_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kntina_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
