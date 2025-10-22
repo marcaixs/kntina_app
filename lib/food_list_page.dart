96fdd4caf9d78775f47c0ade3f1aa5ada2c8db7a
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-22 13:23:49 +0200

    feat: add category selection row

--- lib/food_list_page.dart
+++ lib/food_list_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:kntina_app/food_card.dart';
 import 'package:kntina_app/food_grid.dart';
 import 'package:kntina_app/food_service.dart';
 import 'package:kntina_app/search_page.dart';
@@ -13,6 +12,7 @@ class FoodListPage extends StatefulWidget {
 
 class _FoodListPageState extends State<FoodListPage> {
   List _foodList = [];
+  List _filteredList = [];
   bool _isLoading = true;
 
   @override
@@ -22,17 +22,29 @@ class _FoodListPageState extends State<FoodListPage> {
   }
 
   void _fetchFood() async {
-  setState(() => _isLoading = true);
-  try {
-    final data = await FoodService.fetchFood();
-    setState(() {
-      _foodList = data;
-      _isLoading = false;
-    });
-  } catch (e) {
-    setState(() => _isLoading = false);
+    setState(() => _isLoading = true);
+    try {
+      final data = await FoodService.fetchFood();
+      setState(() {
+        _foodList = data;
+        _filteredList = _foodList;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() => _isLoading = false);
+    }
   }
-}
+
+  void filterContent(String categoryInput) {
+    List filteredContent = _foodList.where((item) {
+      final categoryName = item['category']['name'];
+      return categoryName.contains(categoryInput);
+    }).toList();
+
+    setState(() {
+      _filteredList = filteredContent;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,21 +52,14 @@ class _FoodListPageState extends State<FoodListPage> {
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            radius: 18,
-          ),
+          child: CircleAvatar(radius: 18),
         ),
-        title: Center(
-          child: Image.asset(
-            'assets/images/logo.png', 
-            height: 25, 
-          ),
-        ),
+        title: Center(child: Image.asset('assets/images/logo.png', height: 25)),
         actions: [
           IconButton(
             icon: const Icon(Icons.search, color: Colors.black),
             onPressed: () {
-               Navigator.push(
+              Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (_) => SearchPage(foodList: _foodList),
@@ -64,9 +69,64 @@ class _FoodListPageState extends State<FoodListPage> {
           ),
         ],
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Expanded(child: FoodGrid(foodList: _foodList))
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _filteredList = _foodList;
+                      });
+                    },
+                    icon: Icon(Icons.local_dining, color: Colors.black),
+                  ),
+                  Text('Todo')
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      filterContent('Pasta');
+                    },
+                    icon: Icon(Icons.dining_sharp, color: Colors.black),
+                  ),
+                  Text('Pasta')
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      filterContent('Verduras');
+                    },
+                    icon: Icon(Icons.dining_sharp, color: Colors.black),
+                  ),
+                  Text('Verduras')
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      filterContent('Pescado');
+                    },
+                    icon: Icon(Icons.dining_sharp, color: Colors.black),
+                  ),
+                  Text('Pescado')
+                ],
+              ),
+            ],
+          ),
+          _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Expanded(child: FoodGrid(foodList: _filteredList)),
+        ],
+      ),
     );
   }
 }
