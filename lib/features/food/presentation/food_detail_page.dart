6a83b3942523fc6d4d284d673e3e39e60f0cbbf1
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-25 20:13:42 +0200

    refactor: update layout of FoodDetailPage

--- lib/features/food/presentation/food_detail_page.dart
+++ lib/features/food/presentation/food_detail_page.dart
@@ -3,27 +3,58 @@ import 'package:flutter/material.dart';
 class FoodDetailPage extends StatelessWidget {
   final Map food;
   final Function addToCart;
-  const FoodDetailPage({super.key, required this.food, required this.addToCart});
+  const FoodDetailPage({
+    super.key,
+    required this.food,
+    required this.addToCart,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
                 children: [
-                  Image.network(food['images'][0]),
-                  Image.network(food['images'][1]),
-                  Image.network(food['images'][2]),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      height: 600,
+                      child: PageView(
+                        children: [
+                          Image.network(food['images'][0], fit: BoxFit.cover),
+                          Image.network(food['images'][1], fit: BoxFit.cover),
+                          Image.network(food['images'][2], fit: BoxFit.cover),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(food['title'], style: TextStyle(fontSize: 40)),
+                      Text(
+                        food['price'].toString(),
+                        style: TextStyle(fontSize: 30, color: Colors.green),
+                      ),
+                    ],
+                  ),
+
+                  Text(food['description'], textAlign: TextAlign.justify,),
                 ],
               ),
-            ),
-            Text(food['price'].toString()),
-            Text(food['description']),
-            ElevatedButton(onPressed: (){addToCart(food);}, child: Text('Añadir a la cesta')),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  addToCart(food);
+                },
+                child: Text('Añadir a la cesta'),
+              ),
+            ],
+          ),
         ),
       ),
     );
