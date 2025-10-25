261cb865d3c0664970359556b18730cfb8183cfd
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-25 12:39:20 +0200

    feat: render a listview of items in cart

--- lib/features/cart/widgets/cart_list.dart
+++ lib/features/cart/widgets/cart_list.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:kntina_app/features/cart/widgets/cart_food_card.dart';
 
 class CartList extends StatelessWidget {
-  const CartList({super.key});
+  final List cartList;
+  const CartList({super.key, required this.cartList});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: cartList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return CartFoodCard(item: cartList[index]);
+        },
+      ),
+    );
   }
 }
