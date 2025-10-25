beaa08a0290bcf687b824fb9762236591a5a61a1
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-25 12:39:47 +0200

    feat: show data from each item in cart

--- lib/features/cart/widgets/cart_food_card.dart
+++ lib/features/cart/widgets/cart_food_card.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CartFoodCard extends StatelessWidget {
-  const CartFoodCard({super.key});
+  final item;
+  const CartFoodCard({super.key, this.item});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Row(
+        children: [
+          Image.network(item['images'][0],  width: 100,
+  height: 100, fit: BoxFit.cover),
+          Column(
+            children: [
+              Text(item['title']),
+              Text(item['price'].toString()),
+              Row(
+                children: [
+                  IconButton(icon: Icon(Icons.cancel), onPressed: () {}),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
