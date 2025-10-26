7330e28d699887d09b8bcfd9c8c06b8dc210665e
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 14:03:43 +0100

    feat: add login page to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kntina_app/features/auth/presentation/login_page.dart';
 import 'package:kntina_app/features/home/home_page.dart';
 
 void main() {
@@ -10,6 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home:HomePage());
+    return const MaterialApp(home:LoginPage());
   }
 }
