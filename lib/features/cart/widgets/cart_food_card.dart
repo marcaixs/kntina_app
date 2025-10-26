9ea1308c8a1f449175ded85c9e783b83348f78a4
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 16:40:03 +0100

    style: add new style to cartFoodCart

--- lib/features/cart/widgets/cart_food_card.dart
+++ lib/features/cart/widgets/cart_food_card.dart
@@ -3,27 +3,50 @@ import 'package:flutter/material.dart';
 class CartFoodCard extends StatelessWidget {
   final item;
   const CartFoodCard({super.key, this.item});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Image.network(item['images'][0],  width: 100,
-  height: 100, fit: BoxFit.cover),
-          Column(
-            children: [
-              Text(item['title']),
-              Text(item['price'].toString()),
-              Row(
-                children: [
-                  IconButton(icon: Icon(Icons.cancel), onPressed: () {}),
-                ],
+    return  SizedBox(
+        height: 230,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                item['images'][0],
+                width: 180,
+                height: 230,
+                fit: BoxFit.cover,
               ),
-            ],
-          ),
-        ],
-      ),
-    );
+            ),
+            Expanded(
+              child: 
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item['title'],
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          '${item['price'].toString()} €',
+                          style: TextStyle(fontSize: 10, color: Colors.green),
+                        ),
+                      ],
+                    ),
+                  
+                    IconButton(icon: Icon(Icons.cancel), onPressed: () {}),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      );
   }
 }
