6850fc4e36d1dbeaa6bbbdcccb145873c2b1b1eb
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 17:36:23 +0100

    style: change style and add info from user

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kntina_app/features/profile/change_password_page.dart';
 import 'package:kntina_app/features/profile/edit_profile_page.dart';
 import 'package:kntina_app/features/profile/information_page.dart';
+import 'package:kntina_app/user.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -22,141 +23,205 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
         title: Image.asset('assets/images/logo.png', height: 25),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 50),
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  width: 360,
-                  height: 360,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/profile_frame.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-
-                Positioned(
-                  top: -50,
-                  left: 0,
-                  right: 0,
-                  child: CircleAvatar(
-                    radius: 100,
-                    backgroundImage: AssetImage('assets/images/profile.jpg'),
-                  ),
-                ),
-
-                Positioned(
-                  top: 250,
-                  left: 0,
-                  right: 0,
-                  height: 100,
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Nombre del Usuario',
-                              style: TextStyle(color: Colors.green),
-                            ),
-                            Text(
-                              'Nombre del Usuario',
-                              style: TextStyle(color: Colors.green),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Nombre del Usuario',
-                              style: TextStyle(color: Colors.green),
-                            ),
-                            Text(
-                              'Nombre del Usuario',
-                              style: TextStyle(color: Colors.green),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 60),
+              Stack(
+                clipBehavior: Clip.none,
                 children: [
-                  ListTile(
-                    leading: Icon(Icons.edit),
-                    title: Text('Editar perfil'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (_) => EditProfilePage()),
-                      );
-                    },
+                  Container(
+                    width: double.infinity,
+                    height: 380,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/profile_frame.png'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.lock),
-                    title: Text('Cambiar contraseña'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (_) => ChangePasswordPage()),
-                      );
-                    },
+
+                  Positioned(
+                    top: -50,
+                    left: 0,
+                    right: 0,
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage: AssetImage('assets/images/profile.jpg'),
+                    ),
                   ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.gavel),
-                    title: Text('Términos y condiciones'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () {},
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.shield),
-                    title: Text('Política de privacidad'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () {},
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Información'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (_) => InformationPage()),
-                      );
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.logout),
-                    title: Text('Cerrar sesión'),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: () {},
+
+                  Positioned(
+                    top: 240,
+                    left: 0,
+                    right: 0,
+                    height: 120,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.mail, color: Colors.green),
+                                    SizedBox(width: 10),
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 2,
+                                        vertical: 2,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                          20,
+                                        ), 
+                                        border: Border.all(
+                                          color:
+                                              Colors.green,
+                                          width: 2, 
+                                        ),
+                                      ),
+                                      child: Text(
+                                        'CORREO',
+                                        style: TextStyle(color: Colors.green),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  testUser.email,
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.phone, color: Colors.green),
+                                    SizedBox(width: 10),
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 2,
+                                        vertical: 2,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                          20,
+                                        ), 
+                                        border: Border.all(
+                                          color:
+                                              Colors.green,
+                                          width: 2, 
+                                        ),
+                                      ),
+                                      child: Text(
+                                        'CONTACTO',
+                                        style: TextStyle(color: Colors.green),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  testUser.phone,
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      dense: true,
+                      visualDensity: VisualDensity.compact,
+                      leading: Icon(Icons.edit),
+                      title: Text('Editar perfil'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (_) => EditProfilePage()),
+                        );
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      dense: true,
+                      visualDensity: VisualDensity.compact,
+                      leading: Icon(Icons.lock),
+                      title: Text('Cambiar contraseña'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => ChangePasswordPage(),
+                          ),
+                        );
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      dense: true,
+                      visualDensity: VisualDensity.compact,
+                      leading: Icon(Icons.gavel),
+                      title: Text('Términos y condiciones'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {},
+                    ),
+                    Divider(),
+                    ListTile(
+                      dense: true,
+                      visualDensity: VisualDensity.compact,
+                      leading: Icon(Icons.shield),
+                      title: Text('Política de privacidad'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {},
+                    ),
+                    Divider(),
+                    ListTile(
+                      dense: true,
+                      visualDensity: VisualDensity.compact,
+                      leading: Icon(Icons.abc),
+                      title: Text('Información'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (_) => InformationPage()),
+                        );
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      dense: true,
+                      visualDensity: VisualDensity.compact,
+                      leading: Icon(Icons.logout),
+                      title: Text('Cerrar sesión'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
