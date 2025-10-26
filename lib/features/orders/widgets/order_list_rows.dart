aa5947ee4ca5078a9133e47ca654945bda652125
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 12:25:40 +0100

    feat: add orderslistrows widget

---
+++ lib/features/orders/widgets/order_list_rows.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:kntina_app/features/orders/presentation/oder_details_page.dart';
+
+
+class OrderListRows extends StatelessWidget {
+  final Map order;
+  const OrderListRows({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+           
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(order['date']),
+                  Text(order['id']),
+                ],
+              ),
+              const SizedBox(height: 8), 
+          
+                
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('${order['price']} €'),
+                  Text(order['status'], style: TextStyle(color: Colors.green)),
+                ],
+              ),
+              const SizedBox(height: 12), 
+              SizedBox(
+                height: 200, 
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: order['products'].length,
+                  itemBuilder: (context, index) {
+                    final product = order['products'][index];
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                           Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => OrderDetailsPage(product: product),
+                            ),
+                          ); 
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.network(
+                            product['images'][0],
+                            width: 150,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          const Divider(thickness: 1, color: Colors.grey), 
+        ],
+      ),
+    );
+  }
+}
+
