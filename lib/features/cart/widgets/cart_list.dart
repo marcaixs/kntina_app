33b5d6c4923ff1caf8c825c10aeea04f880cd9d1
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 22:37:59 +0100

    feat: pass update and remove functions to child widget

--- lib/features/cart/widgets/cart_list.dart
+++ lib/features/cart/widgets/cart_list.dart
@@ -3,21 +3,35 @@ import 'package:kntina_app/features/cart/widgets/cart_food_card.dart';
 
 class CartList extends StatelessWidget {
   final List cartList;
-  const CartList({super.key, required this.cartList});
+  final Function(int, int)? updateQuantity;
+  final Function(int)? removeItem;
+
+  const CartList({
+    super.key,
+    required this.cartList,
+    this.updateQuantity,
+    this.removeItem,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: cartList.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Column(
-          children: [
-            CartFoodCard(item: cartList[index]),
-            SizedBox(height: 20,)
-          ],
-        );
-      },
+    return Container(
+      child: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: cartList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              CartFoodCard(
+                item: cartList[index],
+                updateQuantity: updateQuantity,
+                removeItem: removeItem,
+              ),
+              SizedBox(height: 20),
+            ],
+          );
+        },
+      ),
     );
   }
 }
