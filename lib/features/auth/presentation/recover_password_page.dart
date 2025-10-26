c812b71b9d331dfcfc7a836a86b87d1775de5b03
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 20:08:58 +0100

    feat: add global key to recover password form

--- lib/features/auth/presentation/recover_password_page.dart
+++ lib/features/auth/presentation/recover_password_page.dart
@@ -4,10 +4,14 @@ import 'package:kntina_app/features/shared/widgets/custom_button.dart';
 import 'package:kntina_app/features/shared/widgets/custom_text_field.dart';
 
 class RecoverPasswordPage extends StatelessWidget {
+  
   const RecoverPasswordPage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+   final _formRecoverPasswordKey = GlobalKey<FormState>();
+   
     return Scaffold(
       appBar: BackAppbar(title: 'Recuperar contraseña'),
       body: Center(
@@ -19,21 +23,24 @@ class RecoverPasswordPage extends StatelessWidget {
                 SizedBox(height: 100),
                 Image(image: AssetImage("assets/images/logo.png"), width: 200),
                 SizedBox(height: 50),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: CustomTextField(
-                    label: 'Correo',
-                    icon: Icons.mail,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Por favor introduce tu correo electrónico";
-                      }
-                      if (!value.contains('@') || !value.contains('.')) {
-                        return "Por favor introduce un correo electrónico válido";
-                      }
-                      return null;
-                    },
+                Form(
+                  key: _formRecoverPasswordKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: CustomTextField(
+                      label: 'Correo',
+                      icon: Icons.mail,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Por favor introduce tu correo electrónico";
+                        }
+                        if (!value.contains('@') || !value.contains('.')) {
+                          return "Por favor introduce un correo electrónico válido";
+                        }
+                        return null;
+                      },
+                    ),
                   ),
                 ),
               ],
