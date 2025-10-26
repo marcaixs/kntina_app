c8825fdb882c4f6dc52b4e9a1f2ad6ce02d5c58d
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 17:11:07 +0100

    feat: add icon buttons

--- lib/features/cart/widgets/cart_food_card.dart
+++ lib/features/cart/widgets/cart_food_card.dart
@@ -5,48 +5,65 @@ class CartFoodCard extends StatelessWidget {
   const CartFoodCard({super.key, this.item});
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-        height: 230,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                item['images'][0],
-                width: 180,
-                height: 230,
-                fit: BoxFit.cover,
-              ),
+    return SizedBox(
+      height: 230,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.network(
+              item['images'][0],
+              width: 180,
+              height: 230,
+              fit: BoxFit.cover,
             ),
-            Expanded(
-              child: 
+          ),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
                 Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          item['title'],
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          '${item['price'].toString()} €',
-                          style: TextStyle(fontSize: 10, color: Colors.green),
-                        ),
-                      ],
+                    Text(
+                      item['title'],
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  
-                    IconButton(icon: Icon(Icons.cancel), onPressed: () {}),
+                    SizedBox(height: 8),
+                    Text(
+                      '${item['price'].toString()} €',
+                      style: TextStyle(fontSize: 10, color: Colors.green),
+                    ),
                   ],
                 ),
-              ),
-          ],
-        ),
-      );
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(icon: Icon(Icons.cancel), onPressed: () {}),
+                    Row(
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.remove)),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text('2', style: TextStyle(fontSize: 16)),
+                        ),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
