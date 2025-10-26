5d2dcdc10634d61a47706424d4278465baf03e4d
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 12:24:55 +0100

    feat: add orders_page

---
+++ lib/features/orders/presentation/orders_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:kntina_app/features/orders/widgets/order_list_rows.dart';
+
+class OrdersPage extends StatefulWidget {
+  final List orderList;
+  const OrdersPage({super.key, required this.orderList});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: widget.orderList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return OrderListRows(order: widget.orderList[index],);
+        },
+      ),
+    );
+  }
+}
