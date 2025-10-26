bf9dc65c64bac753fd911db75de91f75cd0d9aa7
Marc <147423007+marcaixs@users.noreply.github.com>
2025-10-26 12:36:15 +0100

    feat: set all row clickable and rout to details page

--- lib/features/orders/widgets/order_list_rows.dart
+++ lib/features/orders/widgets/order_list_rows.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kntina_app/features/orders/presentation/oder_details_page.dart';
 
-
 class OrderListRows extends StatelessWidget {
   final Map order;
   const OrderListRows({super.key, required this.order});
@@ -12,46 +11,44 @@ class OrderListRows extends StatelessWidget {
       padding: const EdgeInsets.all(12.0),
       child: Column(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-           
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(order['date']),
-                  Text(order['id']),
-                ],
-              ),
-              const SizedBox(height: 8), 
-          
-                
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('${order['price']} €'),
-                  Text(order['status'], style: TextStyle(color: Colors.green)),
-                ],
-              ),
-              const SizedBox(height: 12), 
-              SizedBox(
-                height: 200, 
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: order['products'].length,
-                  itemBuilder: (context, index) {
-                    final product = order['products'][index];
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: GestureDetector(
-                        onTap: () {
-                           Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => OrderDetailsPage(product: product),
-                            ),
-                          ); 
-                        },
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => OrderDetailsPage(order: order),
+                ),
+              );
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text(order['date']), Text(order['id'])],
+                ),
+                const SizedBox(height: 8),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('${order['price']} €'),
+                    Text(
+                      order['status'],
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: order['products'].length,
+                    itemBuilder: (context, index) {
+                      final product = order['products'][index];
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(12),
                           child: Image.network(
@@ -61,17 +58,16 @@ class OrderListRows extends StatelessWidget {
                             fit: BoxFit.cover,
                           ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-          const Divider(thickness: 1, color: Colors.grey), 
+          const Divider(thickness: 1, color: Colors.grey),
         ],
       ),
     );
   }
 }
-
