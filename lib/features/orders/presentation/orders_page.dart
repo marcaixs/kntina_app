3776cae2bd073f3d9bb8d4e7fd2b2878d747f566
Marc <147423007+marcaixs@users.noreply.github.com>
2025-11-03 19:24:03 +0100

    refactor: convert OrdersPage from StatefulWidget to StatelessWidget

--- lib/features/orders/presentation/orders_page.dart
+++ lib/features/orders/presentation/orders_page.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:kntina_app/features/orders/widgets/order_list_rows.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   final List orderList;
+
   const OrdersPage({super.key, required this.orderList});
 
   @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: widget.orderList.length,
+      itemCount: orderList.length,
       itemBuilder: (BuildContext context, int index) {
-        return OrderListRows(order: widget.orderList[index],);
+        return OrderListRows(order: orderList[index]);
       },
     );
   }
